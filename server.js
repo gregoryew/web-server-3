7ef222f5bcb340ccf38127d72041016d88d8c4a3
Gregory Williams <gregoryew@gmail.com>
2017-04-24 00:35:29 -0400

    update port for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
@@ -12,6 +13,6 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 app.use(express.static(__dirname + '/public'));
 console.log(__dirname);
 
-app.listen(3000, function () {
-	console.log('Express server started!');
+app.listen(PORT, function () {
+	console.log('Express server started! on port ' + PORT);
 });
